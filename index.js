e6b0ea6b9046cf15d01877af51c3543649726ae5
Jared S <jaredjames2020@gmail.com>
2020-03-29 23:16:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ let newHeader = document.createElement('h1')
 document.body.appendChild(newHeader)
 
 let newHeader = document.querySelector("h1#victory");
-newHeader.innerHTML = ""
+newHeader.innerHTML = "\"YOUR-NAME is the champion\""

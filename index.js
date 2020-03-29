ca940bcca3052ae1e994de576e2316b777dbfb74
Jared S <jaredjames2020@gmail.com>
2020-03-29 23:21:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 
 main.remove()
 
-let newHeader = document.createElement('h1')
+let newHeader = document.createElement('h1#victory')
 document.body.appendChild(newHeader)
-document.createElement('h1#victory')
+
 
 newHeader.innerHTML = "\"YOUR-NAME is the champion\""
